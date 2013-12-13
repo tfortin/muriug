fbcff4e130be4d88143030779920933d2682539d
Muriug Webadmin <webadmin@muriug>
2013-12-13 16:04:05 +0100

    First tests with bootstrap

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
-<?php
-	header('Location: ../index.php');
-?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Muriug apps</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link href="../css/bootstrap.css" rel="stylesheet" />
+		<link href="../css/bootstrap-responsive.css" rel="stylesheet" />
+		<link rel="shortcut icon" href="../resources/muriug.ico">
+	</head>
+	<body>
+		<!--<h1>Muriug apps</h1>-->
+		<div class="navbar">
+			<div class="navbar-inner">
+				<a class="brand" href="#">Muriug apps</a>
+				<ul class="nav">
+					<li class="active"><a href="#">Home</a></li>
+				</ul>
+			</div>
+		</div>
+		<script src="../js/jquery.js"></script>
+		<script src="../js/bootstrap.js"></script>
+	</body>
+</html>
